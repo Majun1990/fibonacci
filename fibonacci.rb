ccbf3b081a116d7cdbff0004b67651f23033ba68
Majun1990 <merijun_a@hotmail.com>
2019-09-20 00:09:56 +0000

    completed test

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,23 +1,21 @@
-module Fibonacci
-    def iterative_fib(num)
-        index = num
-        fib1 = 0
-        fib2 = 1
-        index.times do 
-            fibo = fib1 + fib2
-            fib1 = fib2
-            fib2 = fibo
-        end
-        return fibo
-    end
+def iterative_fib(num)
+  index = num
+  fib1 = 0
+  fib2 = 1
+  index.times do 
+    fibo = fib1 + fib2
+    fib1 = fib2
+    fib2 = fibo
+  end
+    puts fib1
+end
 
-    def recursive_fib(num)
-        if num < 2
-            num
-        else
-            fibo(num - 1) + fibo(num - 2)
-        end
-    end
+def recursive_fib(num)
+  if num < 2
+    num
+  else
+    recursive_fib(num - 1) + recursive_fib(num - 2)
+  end
 end
 
 require 'benchmark'
@@ -26,3 +24,4 @@ Benchmark.bm do |x|
   x.report("recursive_fib") { recursive_fib(num) }
   x.report("iterative_fib")  { iterative_fib(num)  }
 end
+    
